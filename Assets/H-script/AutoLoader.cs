6600fa262885070d7756d07bdc70b48cd97cfa90
Hasina <Hasina.haidari18@gmail.com>
2018-01-28 16:48:14 +0430

    firstafghangirlsgamedevelopers

---
+++ Assets/H-script/AutoLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoLoader : MonoBehaviour {
+
+	int counter=0;
+	public int loadTime;
+	public int scene;
+
+	// Use this for initialization
+	void Start(){
+		InvokeRepeating ("Timer",0.001f,1f);
+	}
+	// Update is called once per frame
+	void Update(){
+		if(counter == loadTime){
+			SceneLoader (scene);
+		}
+	}
+	void Timer(){
+		counter++;
+	}
+
+	void SceneLoader(int scene){
+		SceneManager.LoadScene (scene);
+	}
+
+
+}
---
+++ Assets/H-script/ButtonController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Button SoundButton;
+	public Button BackButton;
+	public Button ExitButton;
+
+	public Animator SoundAnimator;
+	public Animator BackAnimator;
+	public Animator ExitAnimator;
+
+	bool isActive;
+
+
+
+	void Start () {
+
+		isActive = false;
+		SoundAnimator = SoundButton.GetComponent<Animator> ();
+		BackAnimator = BackButton.GetComponent<Animator> ();
+		ExitAnimator = ExitButton.GetComponent<Animator> ();
+
+	}
+
+
+	public void ActiveAnimation(){
+
+		if(!isActive){
+			
+		SoundButton.gameObject.SetActive (true);
+		BackButton.gameObject.SetActive (true);
+		ExitButton.gameObject.SetActive (true);
+
+		SoundAnimator.SetBool ("sound",true);
+		BackAnimator.SetBool ("back",true);
+		ExitAnimator.SetBool ("exit",true);
+
+		isActive = true;
+
+	}
+
+		else {
+
+			SoundButton.gameObject.SetActive (false); 
+			BackButton.gameObject.SetActive (false);
+			ExitButton.gameObject.SetActive (false);
+
+			SoundAnimator.SetBool ("sound",false);
+			BackAnimator.SetBool ("back",false);
+			ExitAnimator.SetBool ("exit",false);
+
+			isActive = false;
+
+		}
+}
+}
